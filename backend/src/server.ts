a045b37d8f16c63f2b13a745fa63385c9bebf296
ColeUyematsu <cjuyematsu@gmail.com>
2025-07-02 14:43:18 -0700

    Add signin and signup routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import authRoutes from './routes/AuthRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello, Express with TypeScript!");

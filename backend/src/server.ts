9824a19febc0266c41a6e97cbf4bae76f6bec5d1
Vadym Musiienko <musiienkovadym@gmail.com>
2025-07-09 11:54:45 -0700

    Remove pgadmin, create db tables (schema) and populate locations with airports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,18 +2,21 @@ import express from "express";
 import authRoutes from "./routes/AuthRoutes";
 import { initializeDatabase } from "../lib/initialize_db";
 import tripRoutes from "./routes/Trips";
+import getAirports from "../lib/airports";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/api/auth", authRoutes);
 
 async function startServer() {
     try {
-        await initializeDatabase();
+        await initializeDatabase(); // Initialize the database (schema, tables, etc.)
+
+        await getAirports(); // Import airport data
 
         // Mount routes
+        //app.use("/api/auth", authRoutes);
         app.use("/api/trips", tripRoutes);
 
         app.listen(PORT, () => {

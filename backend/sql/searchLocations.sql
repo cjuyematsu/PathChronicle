1baf169c798ab4d9fa62750357ac126add08c941
Vadym Musiienko <musiienkovadym@gmail.com>
2025-07-09 18:06:26 -0700

    Sql format

--- backend/sql/searchLocations.sql
+++ backend/sql/searchLocations.sql
@@ -12,25 +12,40 @@ WITH ranked_results AS (
         station_code,
         location_type,
         MIN(
-            CASE
-                WHEN LOWER(airport_code) = LOWER($1) THEN 1
-                WHEN LOWER(name) = LOWER($1) THEN 2
-                WHEN LOWER(city) = LOWER($1) THEN 3
-                WHEN LOWER(country) = LOWER($1) THEN 4
-                WHEN LOWER(airport_code) LIKE LOWER($1) || '%' THEN 5
-                WHEN LOWER(name) LIKE LOWER($1) || '%' THEN 6
-                WHEN LOWER(city) LIKE LOWER($1) || '%' THEN 7
-                WHEN LOWER(country) LIKE LOWER($1) || '%' THEN 8
-                ELSE 9
-            END
-        ) AS relevance_score
+            CASE WHEN LOWER(airport_code) = LOWER($1) THEN
+                1
+            WHEN LOWER(name) = LOWER($1) THEN
+                2
+            WHEN LOWER(city) = LOWER($1) THEN
+                3
+            WHEN LOWER(country) = LOWER($1) THEN
+                4
+            WHEN LOWER(airport_code)
+            LIKE LOWER($1) || '%' THEN
+                5
+            WHEN LOWER(name)
+            LIKE LOWER($1) || '%' THEN
+                6
+            WHEN LOWER(city)
+            LIKE LOWER($1) || '%' THEN
+                7
+            WHEN LOWER(country)
+            LIKE LOWER($1) || '%' THEN
+                8
+            ELSE
+                9
+            END) AS relevance_score
     FROM
         locations
     WHERE
-        LOWER(airport_code) LIKE '%' || LOWER($1) || '%'
-        OR LOWER(name) LIKE '%' || LOWER($1) || '%'
-        OR LOWER(city) LIKE '%' || LOWER($1) || '%'
-        OR LOWER(country) LIKE '%' || LOWER($1) || '%' -- $3 will be the type, if present
+        LOWER(airport_code)
+        LIKE '%' || LOWER($1) || '%'
+        OR LOWER(name)
+        LIKE '%' || LOWER($1) || '%'
+        OR LOWER(city)
+        LIKE '%' || LOWER($1) || '%'
+        OR LOWER(country)
+        LIKE '%' || LOWER($1) || '%' -- $3 will be the type, if present
         -- caller should dynamically append "AND location_type = $3" if needed
     GROUP BY
         id,
@@ -53,10 +68,11 @@ FROM
     ranked_results
 ORDER BY
     relevance_score ASC,
-    CASE
-        WHEN relevance_score <= 4 THEN 0
-        ELSE LENGTH(name)
+    CASE WHEN relevance_score <= 4 THEN
+        0
+    ELSE
+        LENGTH(name)
     END ASC,
     name ASC
-LIMIT
-    $2;
+LIMIT $2;
+
